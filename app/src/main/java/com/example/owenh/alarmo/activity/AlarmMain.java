d3584d8620733ebbe0bf7cb6163e4d834f6623b7
huangshaohua <huangshaohuamail@gmail.com>
2017-05-29 21:00:08 +0800

    优化Alarmo特色工具类RingUtil

--- app/src/main/java/com/example/owenh/alarmo/activity/AlarmMain.java
+++ app/src/main/java/com/example/owenh/alarmo/activity/AlarmMain.java
@@ -1,7 +1,6 @@
 package com.example.owenh.alarmo.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import com.example.owenh.alarmo.R;
 import com.example.owenh.alarmo.services.RingService;
--- app/src/main/java/com/example/owenh/alarmo/util/AlarmoUtil.java
+++ app/src/main/java/com/example/owenh/alarmo/util/AlarmoUtil.java
@@ -17,31 +17,10 @@ import static android.R.attr.start;
  * Created by owenh on 2016/8/5.
  */
 
-public class RingUtil {
-
-    private MediaPlayer mediaPlayer = new MediaPlayer();
-
-    public void Ring() {
-        try {
-            File file = new File(Environment.getExternalStorageDirectory(),
-                    "ring0.mp3");
-            mediaPlayer.setDataSource((file.getPath()));//指定音频文件
-            mediaPlayer.prepare();//让MediaPlayer进入到准备状态
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void ringStartOrStop() {
-        if (!mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-        } else {
-            mediaPlayer.reset();
-        }
-    }
+public class AlarmoUtil {
 
     public static boolean isRing(String time) {
-        Log.d("huangshaohua", "2 " + time);
+        //判断是否需要响铃
         DBManager.getInstance().openDatabase();
         final SQLiteDatabase db = DBManager.getInstance().getDatabase();
         Cursor cursor = db.rawQuery("select isRing from SelectTimes where time = ?", new String[]{
@@ -49,10 +28,8 @@ public class RingUtil {
         });
         int b = 0;
         if (cursor.moveToFirst()) {
-            Log.d("huangshaohua", "3 " + time);
             b = cursor.getInt(cursor.getColumnIndex("isRing"));
         }
-        Log.d("huangshaohua", "4 " + (b > 0));
         return b > 0;
     }
 }
